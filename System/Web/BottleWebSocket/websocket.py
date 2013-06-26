17008524f7fbd922b74690fdb1ba99e04770ea93
Damien PUIG <damien.puig@gmail.com>
2013-06-26 02:04:16 +0200

    bottle web socket initialisation delete of tornado web server

---
+++ System/Web/BottleWebSocket/websocket.py
@@ -0,0 +1,23 @@
+from bottle import get, run, template
+from bottle.ext.websocket import GeventWebSocketServer
+from bottle.ext.websocket import websocket
+import time
+
+@get('/')
+def index():
+    return template('index')
+
+@get('/websocket', apply=[websocket])
+def echo(ws):
+    while True:
+        #msg = ws.receive()
+        #if msg is not None:
+           #ws.send(msg)
+        #else: break
+        time.sleep(5)
+        send(ws)
+
+def send(ws):
+	ws.send("go damien")	
+
+run(host='127.0.0.1', port=8080, server=GeventWebSocketServer)
