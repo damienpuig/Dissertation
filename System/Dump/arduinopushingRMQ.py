fc743998988e79ee4b98d4180cbcadcfa27008fb
Damien PUIG <damien.puig@gmail.com>
2013-10-10 18:11:14 +0100

    physical system transmitted using redis pub sub, or RabbitMQ

--- System/Dump/arduinopushingRMQ.py
+++ System/Dump/arduinopushingRMQ.py
@@ -1,37 +1,42 @@
-import sys, os, time, redis, json, datetime, threading, random, pika
+import sys, os, time, redis, json, datetime, threading, random, pika, atexit
 
 # This class simulates the physical network.
 # Once the instance create, it pushes values on
 class Pusher(threading.Thread):
 
-	def __init__(self, iscomand, arduinos=None):
+	def __init__(self, iscomand, arduinos=None, rabbitHost='localhost', queuename='default'):
 		threading.Thread.__init__(self)
 		if arduinos is None:
 			self.arduinos = ["arduino1", "arduino2"]
 		else:
 			self.arduinos = arduinos
 
-				#Possibility to exit OR restart the program
+		#Possibility to exit OR restart the program
 		self.comands = {
 		'stop': lambda:sys.exit(0),  
 		'restart':lambda:restart_program()
 		}
 
 		self.iscomand = iscomand
+		self.rabbith = rabbitHost
+		self.queuename = queuename
 
 
 	#Redis inistialisation, we use the default port.
 	def initRedis(self):
-		self.redisinstance = redis.Redis(host='localhost', port=6379)
+		self.subscriptionintance = redis.Redis(host='localhost', port=6379).pubsub()
 
-		if self.iscomand:
-			self.subscriptionintance = redis.Redis(host='localhost', port=6379).pubsub()
 
-		#If a comand is detected, process the comand
+	def initRabbit(self, host, queuename):
+		self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
+		self.channel = self.connection.channel()
+		self.channel.queue_declare(queue=queuename, durable=True)
+
+
+
 	def processComands(self, data):
 		if data['data'] in self.comands:
 			self.comands[data['data']]()
-
 
 	def checkcomandforever(self):
 		self.subscriptionintance.subscribe(Params.specific_channels["comands.physical"])
@@ -41,21 +46,17 @@ class Pusher(threading.Thread):
 
 
 	def run(self):
-		self.initRedis()
-		print 'Redis connection initialised for thread '
 
 		if self.iscomand:
+			self.initRedis()
 			self.checkcomandforever()
 		else:
-			self.pushforever()
+			self.initRabbit(self.rabbith, self.queuename)
+			self.pushToRabbitForever()
 
 
-	def pushforever(self):
-
-		#check can be used to shutdown the simulation
-		self.check = True
-
-		while self.check:
+	def pushToRabbitForever(self):
+		while True:
 
 			#Get ramdom arduinos
 			dname = random.choice(self.arduinos)
@@ -72,26 +73,26 @@ class Pusher(threading.Thread):
 			deviceLocation = "{\"longitude\": \"40\", \"latitude\": \"30\" }"
 			deviceContext = "{\"nodeId\": \""+ dname +"\", \"location\": "+ deviceLocation +"}"
 
-			# Final message to publish
+			# Final message to send to queue
 			message = "{\"device\": "+ deviceContext +", \"value\": "+ valueContext+"}"
 
+			print 'inserting to RabbitMQ on '+ self.queuename+': '+message
 
-			# Specific channel for the finall message. The channel is patternised
-			channel = Params.specific_channels["physical.arduino.values"].format(dname)
+			#send message to Rabbit
+			self.channel.basic_publish(exchange='',
+                      routing_key=self.queuename,
+                      body=message,
+                      properties=pika.BasicProperties(
+                         delivery_mode = 2, # make message persistent
+                      ))
 
+			time.sleep(5)
+	
+	@atexit.register
+	def onclose():
+		if not self.iscomand:
+			self.connection.close()
 
-			print 'inserting on '+ channel+' list, '+message
-
-			#We push the data on a Redis Key/List.
-			self.redisinstance.rpush(channel, message)
-
-
-			print 'Pushing to redis on ' + channel
-
-			#Publishing on the given channel for notification
-			self.redisinstance.publish(channel,  Params.global_comands['physical_new_message'])
-
-			time.sleep(10)
 
 
 
@@ -101,10 +102,10 @@ if __name__ == '__main__':
 	from Objects.params import Params
 
 	avdevices = ["arduino1", "arduino2", "arduino3", "arduino4"]
-	
 
-	comandListener = Pusher(False, avdevices)
-	arduinoPusher = Pusher(True, avdevices)
+	arduinoPusher = Pusher(False, avdevices, "localhost", Params.specific_channels["physical.arduinos"])
+	comandListener = Pusher(True, avdevices, None, None)
+	 
 
 	comandListener.start()
 	arduinoPusher.start()
